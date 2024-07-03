baf8df0dfd5bd51491ac1af2eccd819ed2b3bec4
04amanrajj <04.aman.raj@gmail.com>
2024-07-03 23:32:46 +0530

    question from video

--- unit-1/sprint-4/day-3/function1.js
+++ unit-1/sprint-4/day-3/function1.js
@@ -1,5 +1,17 @@
 function add(x,y) {
     return x+y;
 }
+function sub(x,y){
+    return x-y
+}
+function div(x,y){
+    return x/y
+}
+function multi(x,y){
+    return x*y
+}
 
 console.log(add(2,5))
+console.log(sub(10,20));
+console.log(div(6,2));
+console.log(multi(3,5));
---
+++ unit-1/sprint-4/day-3/moreFun.js
@@ -0,0 +1,18 @@
+
+function ram(a,b){
+return a+b;
+}
+function shyam(x){
+return x*5;
+}
+function raju(y){
+return y/10;
+}
+function baburao(z){
+return z+10;
+}
+let step1=ram(10,10); 
+let step2=shyam(step1); 
+let step3=raju(step2); 
+let step4=baburao(step3);
+console.log(step4);
