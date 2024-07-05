a735042f507488a00fee286b7a1522857ce607f3
04amanrajj <04.aman.raj@gmail.com>
2024-07-05 14:39:30 +0530

    inbuilt fun

---
+++ unit-1/sprint-4/day-3/inbuiltFun.js
@@ -0,0 +1,17 @@
+// cube root
+let num=10
+console.log(Math.cbrt(num))
+
+// digit before decimal (.)
+
+let num1= 4.3
+console.log(Math.trunc(num1));
+
+// str to Num
+let str= '23421'
+ console.log(Number(str))
+
+// Num to str
+let num2 =123421
+let str2=num2.toString()
+console.log(str2)
---
+++ unit-1/sprint-4/day-3/reverseStrFun.js
@@ -0,0 +1,8 @@
+function reverse(str){
+    let r_str = ''
+    for(let i=str.length-1; i>=0; i--){
+        r_str = r_str + str[i]
+    }
+    console.log(r_str)
+}
+
