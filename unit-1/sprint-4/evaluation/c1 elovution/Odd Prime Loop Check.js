0c8a5201e9b588d6da302969511710d6b4ec2402
04amanrajj <04.aman.raj@gmail.com>
2024-07-06 23:51:31 +0530

    c1 evolution

---
+++ unit-1/sprint-4/evaluation/c1 elovution/Odd Prime Loop Check.js
@@ -0,0 +1,23 @@
+function primeLoopCheck(num) {
+  function isprime(x) {
+    let n = 0;
+    for (let i = 2; i < x; i++) {
+      if (x % i == 0) {
+        n = 1;
+      }
+    }
+    if (n == 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  // 	console.log(isprime(4))
+
+  for (let i = 3; i <= num; i++) {
+    if (isprime(i) == true) {
+      console.log(i);
+    }
+  }
+}
