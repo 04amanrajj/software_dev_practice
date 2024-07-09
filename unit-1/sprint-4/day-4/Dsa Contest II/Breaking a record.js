5ccafa8109806e8094fba2061fc2e7f6a9f33257
04amanrajj <04.aman.raj@gmail.com>
2024-07-09 19:33:41 +0530

    Question of DSA: Breaking a record

---
+++ unit-1/sprint-4/day-4/Dsa Contest II/Breaking a record.js
@@ -0,0 +1,11 @@
+function breakingTheRecord(n, m) {
+    if(n<=m){
+        if(m==n){
+            console.log('Wao')
+        }else{
+            console.log('Broken')
+        }
+    }else{
+        console.log("Not Yet")
+    }
+}
