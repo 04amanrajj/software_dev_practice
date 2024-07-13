ff06c5d9f52692a2f11ea642aee8dc5bc4e523e9
04amanrajj <04.aman.raj@gmail.com>
2024-07-13 17:44:50 +0530

    anonmys fun

---
+++ unit-1/sprint-4/day-4/Coding/higher order fun/evenArr.js
@@ -0,0 +1,18 @@
+// let arr=[1,2,3,4,5,6]
+
+// let even=[]
+
+// for(let i=0; i<arr.length;i++){
+//     if(arr[i]%2==0) even.push(arr[i])
+// }
+
+// console.log(even)
+
+
+let print=function(){
+    console.log("hello");
+}
+
+
+
+print()
