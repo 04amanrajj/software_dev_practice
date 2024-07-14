da513f353e07acbf0753129fa81d488f99b1e836
04amanrajj <04.aman.raj@gmail.com>
2024-07-14 14:35:12 +0530

    traversing 2D array

---
+++ unit-1/sprint-4/day-4/Coding/2D_array/traverse.js
@@ -0,0 +1,16 @@
+//for traverse swap i and j position
+let arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+let bag = "";
+
+for (let i = 0; i < arr1.length; i++) {
+  bag = "";
+  for (let j = 0; j < arr1.length; j++) {
+    bag += arr1[j][i] + " ";
+  }
+  console.log(bag);
+}
