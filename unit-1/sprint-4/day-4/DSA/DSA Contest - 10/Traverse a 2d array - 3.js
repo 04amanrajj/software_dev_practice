08598d51a1d60308f591bff5e63deb1c44529a15
04amanrajj <04.aman.raj@gmail.com>
2024-07-14 16:01:10 +0530

    reverse colon

---
+++ unit-1/sprint-4/day-4/DSA/DSA Contest - 10/Traverse a 2d array - 3.js
@@ -0,0 +1,20 @@
+let Matrix = [
+    [1, 8, 9],
+    [2, 7, 10],
+    [3, 6, 11],
+    [4, 5, 12],
+  ];
+  let Row = 4;
+  let Colon = 3;
+
+function traverse2dArray(N, M, matrix){
+    let bag=''
+    for(let i=M-1;i>=0;i--){
+        for(let j=0;j<N;j++){
+            bag+=matrix[j][i]+" "
+        }
+    }console.log(bag)
+  
+}
+
+traverse2dArray(Row,Colon,Matrix)
