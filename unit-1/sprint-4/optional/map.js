2f974d0dc64004f593b62dc183867b0c88a4b9d1
04amanrajj <04.aman.raj@gmail.com>
2024-07-15 17:22:52 +0530

    for mapping

--- unit-1/sprint-4/optional/map.js
+++ unit-1/sprint-4/optional/map.js
@@ -1,9 +1,41 @@
-const arr = [1, -4, 12, 0, -3, 29, -150];
+// const arr = [1, -4, 12, 0, -3, 29, -150];
 
-let bag = "";
+// let bag = "";
 
-arr.map(function (ele, index) {
-  bag += ele + " ";
-});
+// arr.map(function (ele, index) {
+//   bag += ele + " ";
+// });
 
-console.log(bag);
+// console.log(bag);
+
+const input = [
+  {
+    name: "John",
+    age: 13,
+  },
+  {
+    name: "Mark",
+    age: 56,
+  },
+  {
+    name: "Rachel",
+    age: 45,
+  },
+  {
+    name: "Nate",
+    age: 67,
+  },
+  {
+    name: "Jennifer",
+    age: 65,
+  },
+];
+
+let age = [];
+input.map((element, index) => age.push(element.age));
+
+let min = Math.min(...age);
+let max = Math.max(...age);
+let avg = max - min;
+
+console.log({ age }, { min }, { avg });
