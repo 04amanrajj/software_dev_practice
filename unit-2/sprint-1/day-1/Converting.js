05de956b13d825a1e5c6805898c3a89dbf7d64ef
04amanrajj <04.aman.raj@gmail.com>
2024-07-19 15:53:42 +0530

    Js Input taking

---
+++ unit-2/sprint-1/day-1/Converting.js
@@ -0,0 +1,13 @@
+let x = "10";
+let y = +x;
+
+// console.log(y)
+
+//string to number array
+
+let str = ["1", "2", "3", "5"];
+
+let arr = str.map(Number);
+
+// console.log(arr);
+
---
+++ unit-2/sprint-1/day-1/str-functions.js
@@ -0,0 +1,15 @@
+//trim()
+
+let str = "       this is a computer and a laptop,pc and laptop";
+
+//console.log(str.trim());
+
+//split()
+
+// console.log(str.trim().split(','))
+
+//map()
+
+let num=['1','2','3','4']
+
+console.log(num.map(Number))
