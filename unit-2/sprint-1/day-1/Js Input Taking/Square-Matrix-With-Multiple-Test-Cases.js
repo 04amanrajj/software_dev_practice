716010ed9eef77db55d56a75f4a14a0a8b19b607
04amanrajj <04.aman.raj@gmail.com>
2024-07-25 17:09:23 +0530

    Practice of Taking input in js

---
+++ unit-2/sprint-1/day-1/Js Input Taking/Square-Matrix-With-Multiple-Test-Cases.js
@@ -0,0 +1,53 @@
+function logic(n,mat) {
+    for (let i = 0; i < n; i++) {
+      let bag = "";
+      for (let j = 0; j < n; j++) {
+        bag += mat[i][j] + 1 + " ";
+      }
+      console.log(bag.trim());
+    }
+  }
+  
+  function runProgram(input) {
+    input = input.split("\n");
+  
+    let tc = +input[0];
+  
+    let i = 0;
+  
+     while (tc) {
+      
+      let n = +input[++i];
+  
+      let mat = [];
+      
+      for (let j = 0; j < n; j++) {
+        input[++i] = input[i].trim().split(" ").map(Number);
+        mat.push(input[i]);
+      }
+      logic(n, mat);
+      tc--;
+      
+     }
+   }
+  if (process.env.USERNAME === "") {
+    runProgram(``);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
+  
