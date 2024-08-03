f558c7df79945175e3ce080d715a538c3c592f93
04amanrajj <04.aman.raj@gmail.com>
2024-08-03 17:35:14 +0530

    media-responsive for small screen

--- test.js
+++ test.js
@@ -1,17 +1,4 @@
-function check(arr, k) {
-  let l = 0,
-    r = arr.length - 1,
-    sum = arr[l] + arr[r];
+let arr1=[1,2,3],
+arr2=[4,5,6]
 
-  arr.sort(function (a, b) {
-    return a - b;
-  });
-
-  while (l < r) {
-    if (sum == k) return true;
-    else if (sum < k) l++;
-    else r++;
-  }
-  return false;
-}
-console.log(check([1, 2, 12, 3, 5, 3, 8, 4, 7, 10], 11));
+console.log(...arr2,...arr1);
