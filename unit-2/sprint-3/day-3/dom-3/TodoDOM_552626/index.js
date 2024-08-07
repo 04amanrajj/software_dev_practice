c568852b4ffdf37bddbdf7baf057e095f31920c4
04amanrajj <04.aman.raj@gmail.com>
2024-08-07 19:53:09 +0530

    Basic Todo list

---
+++ unit-2/sprint-3/day-3/dom-3/TodoDOM_552626/index.js
@@ -0,0 +1,30 @@
+document.querySelector("form").addEventListener("submit", myTodo);
+
+function myTodo(event) {
+  event.preventDefault();
+  let taskName = document.querySelector("#task").value;
+  let taskPriority = document.querySelector("#priority").value;
+  let taskObj = {
+    taskName,
+    taskPriority,
+  };
+  displayTable(taskObj);
+}
+
+function displayTable(taskObj) {
+  let row = document.createElement("tr");
+  let td1 = document.createElement("td");
+  td1.innerText = taskObj.taskName;
+  let td2 = document.createElement("td");
+  td2.innerText = taskObj.taskPriority;
+  if (taskObj.taskPriority == "High") {
+    td2.style.backgroundColor = "red";
+  } else {
+    td2.style.backgroundColor = "red";
+  }
+  let td3 = document.createElement("td");
+  td3.innerText = "Add to Fav";
+
+  row.append(td1, td2, td3);
+  document.querySelector("tbody").append(row);
+}
