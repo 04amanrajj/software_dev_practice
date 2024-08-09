df1d2559bcd54ae6f48e074f4154c253bcaadbb4
04amanrajj <04.aman.raj@gmail.com>
2024-08-09 20:01:30 +0530

    todo list with favourite page

--- unit-2/sprint-3/day-3/dom-3/TodoDOM_552626/index.js
+++ unit-2/sprint-3/day-3/dom-3/TodoDOM_552626/index.js
@@ -1,4 +1,7 @@
 document.querySelector("form").addEventListener("submit", myTodo);
+let todoArr = JSON.parse(localStorage.getItem("todo")) || [];
+let favArr = JSON.parse(localStorage.getItem("Favourite")) || [];
+displayTable(todoArr);
 
 function myTodo(event) {
   event.preventDefault();
@@ -8,23 +11,43 @@ function myTodo(event) {
     taskName,
     taskPriority,
   };
-  displayTable(taskObj);
+  todoArr.push(taskObj);
+  localStorage.setItem("todo", JSON.stringify(todoArr));
+
+  displayTable(todoArr);
 }
 
-function displayTable(taskObj) {
-  let row = document.createElement("tr");
-  let td1 = document.createElement("td");
-  td1.innerText = taskObj.taskName;
-  let td2 = document.createElement("td");
-  td2.innerText = taskObj.taskPriority;
-  if (taskObj.taskPriority == "High") {
-    td2.style.backgroundColor = "red";
-  } else {
-    td2.style.backgroundColor = "red";
-  }
-  let td3 = document.createElement("td");
-  td3.innerText = "Add to Fav";
+function displayTable(todoArr) {
+  document.querySelector("tbody").innerHTML = "";
+  for (let i = 0; i < todoArr.length; i++) {
+    let row = document.createElement("tr");
+    let td1 = document.createElement("td");
+    td1.innerText = todoArr[i].taskName;
+    let td2 = document.createElement("td");
+    td2.innerText = todoArr[i].taskPriority;
 
-  row.append(td1, td2, td3);
-  document.querySelector("tbody").append(row);
+    if (todoArr[i].taskPriority == "High") {
+      td2.style.backgroundColor = "red";
+    } else {
+      td2.style.backgroundColor = "green";
+    }
+    let td3 = document.createElement("td");
+    td3.innerText = "Add to Fav";
+    td3.addEventListener("click", function () {
+      favArr.push(todoArr[i]);
+      
+      // if (favArr[i].taskName == todoArr[i].taskName) {
+      //   return alert("Already added");
+      // } else {
+      //   favArr.push(todoArr[i]);
+      // }
+      // console.log(favArr[i].taskName, todoArr[i].taskName);
+
+      localStorage.setItem("Favourite", JSON.stringify(favArr));
+    });
+
+    row.append(td1, td2, td3);
+
+    document.querySelector("tbody").append(row);
+  }
 }
