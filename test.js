1dcc5f5956ac278f235cc3c120e23cff857a2c9f
04amanrajj <04.aman.raj@gmail.com>
2024-08-23 00:11:35 +0530

    stack (nearest smaller element)

--- test.js
+++ test.js
@@ -1,47 +1,17 @@
-function logic(n,arr) {
+let arr = [4, 1, 3, 2, 5];
+let stack = []
+let ans = []
 
-    let l=0,r=n-1
-    while(l<r){
-        let temp=arr[l]
-        arr[l]=arr[r]
-        arr[r]=temp
-      
-        l++,r--
-    }
-    let bag=''
-    for(let i of arr){
-        bag+=i+' '
-    }
-    console.log(bag)
+for (let i = 0; i < arr.length; i++) {
+  while (stack.length != 0 && stack[stack.length - 1] > arr[i]) {
+    stack.pop();
+  }
+  if (stack.length == 0) stack.push(-1);
+  else {
+    ans.push(stack[stack.length - 1]);
+  }
+
+  stack.push(arr[i]);
 }
 
-function runProgram(input) {
-  input = input.trim().split("\n");
-  let line = 0;
-
-    let [size] = input[line++].trim().split(" ").map(Number); // Read size, x, and y
-    let arr = input[line++].trim().split(" ").map(Number); // Read the array
-
-    logic(size,arr); 
-  
-}
-
-if (process.env.USERNAME === "") {
-  runProgram(``);
-} else {
-  process.stdin.resume();
-  process.stdin.setEncoding("ascii");
-  let read = "";
-  process.stdin.on("data", function (input) {
-    read += input;
-  });
-  process.stdin.on("end", function () {
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-  });
-  process.on("SIGINT", function () {
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-    process.exit(0);
-  });
-}
+console.log(ans);
