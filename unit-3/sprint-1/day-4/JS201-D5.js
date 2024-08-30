7e4c3a59c61a0d1bbb96e0c3acf22c9fdde48b00
04amanrajj <04.aman.raj@gmail.com>
2024-08-30 17:03:02 +0530

    use constructor funtion

--- unit-3/sprint-1/day-4/JS201-D5.js
+++ unit-3/sprint-1/day-4/JS201-D5.js
@@ -15,3 +15,37 @@ function Actor(movieName) {
 let a1 = new Actor("Aladdin");
 console.log(a1.name); // should return Will Smith
 console.log(a1.age); // should return 54
+
+// Problem 2
+
+/**
+ * Create a class called Animal
+ * the object created using Animal class is expected to have the following properties:
+ * - name: the value will be passed while calling Animal with the new keyword
+ * 
+ * the object is expected to have following methods
+ * - run(): console logs '<name of the animal> is running'
+ * - stop(): console logs '<name of the animal> has stopped' 
+ * 
+ * extend Animal to create a Rabbit class
+ * - The object created using Rabbit class has a name, it can run & it can stop
+ * - it also has a method called hide which console logs '<name of the animal> has hidden'
+ */
+class Animal{
+  constructor(name){
+    this.name=name
+  }
+  run(){console.log(this.name,"is running")}
+  stop(){console.log(this.name,"is stopped")}
+}
+
+class Rabbit extends Animal{
+  constructor(name){
+    super(name)
+  }
+  hide(){console.log(this.name,"has hidden")}
+}
+
+let r1 = new Rabbit('Ruby');
+r1.hide() // Ruby has hidden
+r1.run() // Ruby is running
