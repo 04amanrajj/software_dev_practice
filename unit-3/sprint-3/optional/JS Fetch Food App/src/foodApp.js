8889819dc03ddf1b691538f4457a450b162a3199
04amanrajj <04.aman.raj@gmail.com>
2024-09-05 01:09:17 +0530

    optional question

---
+++ unit-3/sprint-3/optional/JS Fetch Food App/src/foodApp.js
@@ -0,0 +1,20 @@
+const getCategoriesData = async () => {
+  // code here
+};
+
+const getIngredientData = async () => {
+  // code here
+};
+
+window.onload = function () {
+  //  get the buttons here and add click event
+};
+
+// donot chnage the export statement
+
+if (typeof exports !== "undefined") {
+  module.exports = {
+    getCategoriesData,
+    getIngredientData,
+  };
+}
