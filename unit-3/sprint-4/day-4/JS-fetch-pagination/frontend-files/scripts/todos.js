f02a4ec4ff3386b9a21e25ccef2c06aaab6c2112
04amanrajj <04.aman.raj@gmail.com>
2024-09-14 22:01:42 +0530

    todo with auth

---
+++ unit-3/sprint-4/day-4/JS-fetch-pagination/frontend-files/scripts/todos.js
@@ -0,0 +1,81 @@
+const baseURL = "http://127.0.0.1:9090";
+const recipeIngredientURL = "http://127.0.0.1:9090/recipeIngredients";
+const employeeURL = "http://localhost:9090/employees";
+const todosURL = "http://localhost:9090/todos";
+const userRegisterUrl = "http://localhost:9090/user/register";
+const userLoginUrl = "http://localhost:9090/user/login";
+
+let mainSection = document.getElementById("data-list-wrapper");
+let paginationWrapper = document.getElementById("pagination-wrapper");
+
+let registerUserUsername = document.getElementById("register-user-username");
+let registerUserFirstname = document.getElementById("register-user-firstname");
+let registerUserLastname = document.getElementById("register-user-lastname");
+let registerUserEmail = document.getElementById("register-user-email");
+let registerUserPassword = document.getElementById("register-user-password");
+let registerUserAvatar = document.getElementById("register-user-avatar");
+let registerUserLevel = document.getElementById("register-user-level");
+let registerUserButton = document.getElementById("register-user");
+
+let updateUserUsername = document.getElementById("update-user-username");
+let updateUserFirstname = document.getElementById("update-user-firstname");
+let updateUserLastname = document.getElementById("update-user-lastname");
+let updateUserEmail = document.getElementById("update-user-email");
+let updateUserPassword = document.getElementById("update-user-password");
+let updateUserAvatar = document.getElementById("update-user-avatar");
+let updateUserLevel = document.getElementById("update-user-level");
+let updateUserButton = document.getElementById("update-user");
+
+let loginUserUsername = document.getElementById("login-user-username");
+let loginUserPassword = document.getElementById("login-user-password");
+let loginUserButton = document.getElementById("login-user");
+
+window.addEventListener("load", () => {});
+
+registerUserButton.addEventListener("click", async () => {
+  let obj = {
+    username: registerUserUsername.value,
+    firstname: registerUserFirstname.value,
+    lastname: registerUserLastname.value,
+    email: registerUserEmail.value,
+    password: registerUserPassword.value,
+    userLevel: registerUserLevel.value,
+  };
+  console.log(obj);
+
+  try {
+    let response = await fetch(userRegisterUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    });
+    let data = await response.json();
+    alert(JSON.stringify(data));
+    displayData(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+loginUserButton.addEventListener("click", async () => {
+  let obj = {
+    username: loginUserUsername.value,
+    password: loginUserPassword.value,
+  };
+  console.log(obj)
+  try {
+    let response = await fetch(userLoginUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    });
+    let data=await response.json()
+    let auth =localStorage.setItem("token",data.accessToken)
+  } catch (error) {
+    alert("You don't look familiar")
+  }
+});
