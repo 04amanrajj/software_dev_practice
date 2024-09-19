6afe002eaa17b7f056c5c7e443cc18186d9742b1
04amanrajj <04.aman.raj@gmail.com>
2024-09-19 22:30:04 +0530

    namste js

--- revisit_JS.js
+++ revisit_JS.js
@@ -9,4 +9,23 @@
   name();
   console.log(x);
 }
+// functoins scops
+
+{
+  let x = 1;
+  a();
+  b();
+  console.log(x);
+
+  function a() {
+    let x = 100;
+    console.log(x);
+  }
+
+  function b() {
+    let x = 10;
+    console.log(x);
+  }
+}
+
 
