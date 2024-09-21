ba7c99d4d84e50ab49aba56e8e8b3d7acd5dacaa
04amanrajj <04.aman.raj@gmail.com>
2024-09-21 22:30:49 +0530

    higher order functions

--- higerOrder.js
+++ higerOrder.js
@@ -2,7 +2,7 @@
 let radius = [2, 4, 6, 8];
 
 function areaa(radius) {
-  return Math.PI * radius*radius;
+  return Math.PI * radius * radius;
 }
 
 function diameter(radius) {
@@ -17,5 +17,29 @@ function area(radius, logic) {
   return arr;
 }
 
-console.log(area(radius, areaa));
-console.log(area(radius, diameter));
+// console.log(area(radius, areaa));
+// console.log(area(radius, diameter));
+
+// -------------------MAP---------------------------------------------
+{
+  let arr = [4, 6, 3, 7, 2];
+
+  let output = arr.map((x) => x * 2);
+
+  // console.log(output);
+}
+
+// -------------------Filter---------------------------------------------
+
+let arr = [4, 1, 2, 4, 9, 8];
+
+let output = arr.filter((e) => e > 4);
+// console.log(output);
+
+// -------------------Reduce---------------------------------------------
+let red = arr.reduce((acc, curr) => {
+  if (acc > curr) acc = curr;
+  return acc;
+});
+
+console.log(red);
