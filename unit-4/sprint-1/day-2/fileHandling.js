905d694bd872913632e4443e08ed27453d03912c
04amanrajj <04.aman.raj@gmail.com>
2024-10-13 23:39:49 +0530

    node modules

--- unit-4/sprint-1/day-2/fileHandling.js
+++ unit-4/sprint-1/day-2/fileHandling.js
@@ -1,5 +1,5 @@
-const fs = require("node:fs");
-const process = require("node:process");
+const fs = require("fs");
+const process = require("process");
 
 // create new file (if file not found) and apend the txt
 fs.appendFile("text.txt", "Hello node!\n", function (err) {
@@ -14,6 +14,8 @@ fs.appendFile("text.txt", `${perameters}\n`, (err) => {
   if (err) throw err;
   console.log("user file saved");
 });
+
+
 
 // fs.writeFile() method replaces the specified file and content:
 // fs.writeFile("text.txt", `${perameters}\n`, (err) => {
