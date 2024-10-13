6d705b8b7139927c5b73314effaf38adb5baa404
04amanrajj <04.aman.raj@gmail.com>
2024-10-13 14:33:21 +0530

    node modules: file system, taking permeters from terminal using process.argv

--- unit-4/sprint-1/day-2/argumentCalculator.js
+++ unit-4/sprint-1/day-2/argumentCalculator.js
@@ -1,3 +1,25 @@
-const randomNumber =require("node:crypto")
-console.log(randomNumber.randomInt(10,200));
+const randomNumber = require("node:crypto");
+const process = require("node:process");
+console.log("random Number",randomNumber.randomInt(10, 200));
+let perameters = process.argv.slice(2);
 
+console.log({ perameters });
+
+switch (perameters[0]) {
+  case "add":
+    console.log(+perameters[1] + +perameters[2]);
+    break;
+
+  case "sub":
+    console.log(+perameters[1] - +perameters[2]);
+    break;
+  case "prodcut":
+    console.log(+perameters[1] * +perameters[2]);
+    break;
+  case "devide":
+    console.log(+perameters[1] / +perameters[2]);
+    break;
+  default:
+    console.log("select from add,sub,prodcut,devide");
+    break;
+}
---
+++ unit-4/sprint-1/day-2/fileHandling.js
@@ -0,0 +1,22 @@
+const fs = require("node:fs");
+const process = require("node:process");
+
+// create new file (if file not found) and apend the txt
+fs.appendFile("text.txt", "Hello node!\n", function (err) {
+  if (err) throw err;
+  console.log("Saved!");
+});
+
+// user perameters in file
+let perameters = process.argv.slice(2);
+
+fs.appendFile("text.txt", `${perameters}\n`, (err) => {
+  if (err) throw err;
+  console.log("user file saved");
+});
+
+// fs.writeFile() method replaces the specified file and content:
+// fs.writeFile("text.txt", `${perameters}\n`, (err) => {
+//   if (err) throw err;
+//   console.log("user file saved");
+// });
