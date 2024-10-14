b65a2c66ffb2201654534e760a562a32392b7cd5
04amanrajj <04.aman.raj@gmail.com>
2024-10-14 19:37:52 +0530

    server created,json file loaded on server

--- unit-4/sprint-1/day-4/myServer.js
+++ unit-4/sprint-1/day-4/myServer.js
@@ -3,9 +3,16 @@ const fs = require("fs");
 
 const server = http.createServer((request, response) => {
   if (request.url == "/") {
-    response.end("this is home page");
+    response.setHeader("Content-Type", "text/html");
+    response.end(`
+      <ul style="display:flex;padding:10px">
+        <li style="margin:10px"><a href="#">/home</li>
+        <li style="margin:10px"><a href="/data">/data</li>
+        <li style="margin:10px"><a href="/report">/report</li>
+      </ul>
+      `);
   } else if (request.url == "/data") {
-    fs.readFile("data.json", (err,data) => {
+    fs.readFile("data.json", (err, data) => {
       if (err) {
         response.write(err);
         response.end();
@@ -13,6 +20,9 @@ const server = http.createServer((request, response) => {
         response.end(data);
       }
     });
+  } else if (request.url == "/reports") {
+    response.setHeader("Content-Type", "text/html");
+    response.end("<h1>Nothing to report</h1>");
   } else response.end("404 not found");
 });
 
