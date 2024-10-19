f9061b1cf5d1a7c59bbf1fc508fd94f6bacc0d40
04amanrajj <04.aman.raj@gmail.com>
2024-10-19 20:17:49 +0530

    server created using node express

--- unit-4/sprint-2/day-1/index.js
+++ unit-4/sprint-2/day-1/index.js
@@ -1,2 +1,9 @@
-const express=require("express")
-console.log(express)
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.status(200).send("Home page");
+});
+
+app.listen(3000,()=>{
+    console.log("server running at http://localhost:3000")
+})
