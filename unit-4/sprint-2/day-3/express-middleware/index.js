1c8c642d2afd77ce74830edab0bec104a2a20ffb
04amanrajj <04.aman.raj@gmail.com>
2024-10-23 00:36:39 +0530

    using cors (cross origin resource sahre)

--- unit-4/sprint-2/day-3/express-middleware/index.js
+++ unit-4/sprint-2/day-3/express-middleware/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const teachersRoute = require("./routes/teachers.route");
 const studentRoute = require("./routes/students.route");
+const cors = require("cors");
 const app = express();
 const fs = require("fs");
 
 // middleware
+app.use(cors());
 app.use(express.json());
-
 app.use((req, res, next) => {
-  
   // console.log("body:", req.body);
   // console.log("got request", req.query);
   // time
@@ -16,11 +16,12 @@ app.use((req, res, next) => {
   next();
   const endTime = new Date().getTime();
   // console.log(`Time taken: ${endTime - startTime}s`);
-  
-  let log=`Method: ${req.method}, route: ${req.url}, completion time: ${endTime - startTime}\n`
-  
-  fs.appendFileSync("./log.txt",log,"utf8")
 
+  let log = `Method: ${req.method}, route: ${req.url}, completion time: ${
+    endTime - startTime
+  }\n`;
+
+  fs.appendFileSync("./log.txt", log, "utf8");
 });
 app.use("/teacher", teachersRoute);
 app.use("/student", studentRoute);
