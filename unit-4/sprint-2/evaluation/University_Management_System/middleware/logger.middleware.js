acff7fa9cd2d11fc694d21cc5ea8c5d0f9f2afdd
04amanrajj <04.aman.raj@gmail.com>
2024-10-28 14:57:07 +0530

    winston logger update

--- unit-4/sprint-2/evaluation/University_Management_System/middleware/logger.middleware.js
+++ unit-4/sprint-2/evaluation/University_Management_System/middleware/logger.middleware.js
@@ -1,30 +1,39 @@
 const expressWinston = require("express-winston");
-const { transports, format } = require("winston");
-// custom format
+const { format, transports } = require("winston");
+
 const myFormat = format.printf(({ level, message, meta, timestamp }) => {
-  return `${level} ${message} ${JSON.stringify(meta)} ${timestamp}`;
+  return `${meta.res.statusCode} ${message} ${
+    meta ? JSON.stringify(meta) : ""
+  } ${timestamp}`;
 });
-// middleware
+
 const logger = expressWinston.logger({
   transports: [
-    new transports.Console(), //in terminal
-    //  to save in file
+    new transports.Console({
+    }),
     new transports.File({
       level: "warn",
       filename: "./logs/warnings.log",
-    }),
-    new transports.File({
-      level: "error",
-      filename: "./logs/errors.log",
+      format: format.combine(
+        format((info) => (info.level === "warn" ? info : false))()
+      ),
     }),
     new transports.File({
       level: "info",
       filename: "./logs/info.log",
+      format: format.combine(
+        format((info) => (info.level === "info" ? info : false))()
+      ),
+    }),
+    new transports.File({
+      level: "error",
+      filename: "./logs/errors.log",
+      format: format.combine(
+        format((info) => (info.level === "error" ? info : false))()
+      ),
     }),
   ],
-  // format to use
   format: format.combine(format.json(), format.timestamp(), myFormat),
-  //
   statusLevels: true,
 });
 
