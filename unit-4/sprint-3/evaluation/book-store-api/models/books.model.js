92d83e38dbe92c14e8cf541f7da1f0bb3daddf33
04amanrajj <04.aman.raj@gmail.com>
2024-11-06 17:48:23 +0530

    model structure fix

--- unit-4/sprint-3/evaluation/book-store-api/models/books.model.js
+++ unit-4/sprint-3/evaluation/book-store-api/models/books.model.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+
 const bookSchema = mongoose.Schema({
   title: { type: String, required: true },
   genre: { type: String, required: true },
   price: { type: Number, required: true },
   author: { type: String, required: true },
 });
-const BookModel = mongoose.model("bookshelf", bookSchema);
-module.exports = BookModel;
+const BooksModel = mongoose.model("book", bookSchema);
+
+module.exports = BooksModel;
