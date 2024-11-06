ba3d6198760edb413a1a51536689c3dc3f776d5d
04amanrajj <04.aman.raj@gmail.com>
2024-11-07 00:10:43 +0530

    feature: jwt token

--- unit-4/sprint-4/day-1/authentication/controllers/user.controller.js
+++ unit-4/sprint-4/day-1/authentication/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const UserModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
 exports.newUser = async (req, res) => {
   const payLoad = req.body;
@@ -17,8 +18,26 @@ exports.loginUser = async (req, res) => {
   try {
     const user = await UserModel.findOne({ email, password });
     if (user) {
-      res.status(200).send({ msg: "Login successfull", token: "abc123" });
+      const token = jwt.sign(req.body, "masai"); //auth jwt token
+      res.status(200).send({ msg: "Login successfull", token });
     }
+  } catch (error) {
+    console.log(error.message);
+    res.status(404).send({ error: error.message });
+  }
+};
+
+exports.cartController = (req, res) => {
+  const token = req.headers.authorization;
+  try {
+    jwt.verify(token, "masai", (err, decoded) => { //verify token
+      if (err) {
+        res.status(403).send("login first");
+        console.log({ error: err.message });
+      } else {
+        res.send("...DATA");
+      }
+    });
   } catch (error) {
     console.log(error.message);
     res.status(404).send({ error: error.message });
--- unit-4/sprint-4/day-1/authentication/routes/login.route.js
+++ unit-4/sprint-4/day-1/authentication/routes/login.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const Router = require("express");
 const authRoute = Router();
-const { newUser, loginUser } = require("../controllers/user.controller");
+const { newUser, loginUser, cartController } = require("../controllers/user.controller");
 authRoute.use(express.json());
 
 authRoute.get("/", (req, res) => {
@@ -9,20 +9,6 @@ authRoute.get("/", (req, res) => {
 });
 
 authRoute.post("/register", newUser);
-
 authRoute.post("/login", loginUser);
-
-authRoute.get("/cart", (req, res) => {
-  const token = req.query.token;
-  try {
-    if (token == "abc123") {
-      res.send("...DATa");
-    } else {
-      res.status(403).send("login first");
-    }
-  } catch (error) {
-    console.log(error.message);
-    res.status(404).send({ error: error.message });
-  }
-});
+authRoute.get("/cart", cartController);
 module.exports = authRoute;
