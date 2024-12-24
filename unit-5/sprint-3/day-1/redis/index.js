91247ddaaa3aa9dbbc17e4f0faf2b26bd613ec29
04amanrajj <04.aman.raj@gmail.com>
2024-12-24 22:43:08 +0530

    redis (database)

---
+++ unit-5/sprint-3/day-1/redis/index.js
@@ -0,0 +1,8 @@
+const Redis = require("ioredis");
+
+const redis = new Redis();
+
+
+// redis.set('name' , 'aman raj' ,'EX', 600).then(data => console.log({data}))
+
+// let data = redis.ttl('name').then(data => console.log({data}))
