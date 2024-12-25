da939d5fe82f4d11bb8ec1769114a466d543355e
04amanrajj <04.aman.raj@gmail.com>
2024-12-26 00:29:16 +0530

    redis magic with ioredis pkg

--- unit-5/sprint-3/day-1/redis/index.js
+++ unit-5/sprint-3/day-1/redis/index.js
@@ -1,24 +1,33 @@
 const express = require("express");
 const Redis = require("ioredis");
-
+const axios = require("axios");
 const app = express();
 
+app.use(express.json());
 const redis = new Redis(
   "redis://default:8520@redis-13668.c305.ap-south-1-1.ec2.redns.redis-cloud.com:13668"
 );
 
-redis.set("name", "Aman");
-
-// redis.ttl('name').then(data => console.log({data}))
-// redis.get("name").then((data) => console.log({ data }));
-app.get("/", async (req, res) => {
+app.get("/:id", async (req, res) => {
   try {
-    const response = await redis.get("name");
-    res.send("HI " + response);
+    const { id } = req.params;
+    let cached_data = await redis.get(id);
+
+    if (cached_data == {}) {
+      console.log(JSON.parse(cached_data));
+      return res.status(200).send(JSON.parse(cached_data));
+    }
+
+    let response = await axios.get(`https://api.github.com/users/${id}`);
+    response = response.data;
+    await redis.set(id, JSON.stringify(response), "ex", 5);
+    console.log(response);
+    res.status(200).send({ data: response });
   } catch (error) {
-    res.send("Error: ", error);
+    res.status(500).send(error.message);
   }
 });
+
 app.listen(3000, () => {
   console.log("http://localhost:3000");
 });
