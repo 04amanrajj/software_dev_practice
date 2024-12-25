4f62d9d0283a3f152d2a5623268785f4421488f4
04amanrajj <04.aman.raj@gmail.com>
2024-12-25 23:10:33 +0530

    ioredis small app

--- unit-5/sprint-3/day-1/redis/index.js
+++ unit-5/sprint-3/day-1/redis/index.js
@@ -1,8 +1,24 @@
+const express = require("express");
 const Redis = require("ioredis");
 
-const redis = new Redis();
+const app = express();
 
+const redis = new Redis(
+  "redis://default:8520@redis-13668.c305.ap-south-1-1.ec2.redns.redis-cloud.com:13668"
+);
 
-// redis.set('name' , 'aman raj' ,'EX', 600).then(data => console.log({data}))
+redis.set("name", "Aman");
 
-// let data = redis.ttl('name').then(data => console.log({data}))
+// redis.ttl('name').then(data => console.log({data}))
+// redis.get("name").then((data) => console.log({ data }));
+app.get("/", async (req, res) => {
+  try {
+    const response = await redis.get("name");
+    res.send("HI " + response);
+  } catch (error) {
+    res.send("Error: ", error);
+  }
+});
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
