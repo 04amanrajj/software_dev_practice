0adb0ee0ca596469f5a3b5f2c85159dfe32bc6c2
04amanrajj <04.aman.raj@gmail.com>
2025-01-11 23:34:31 +0530

    crud operation on pg sql db

--- unit-5/sprint-3/evaluation/postgresDB/index.js
+++ unit-5/sprint-3/evaluation/postgresDB/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
 const knex = require("knex")({
   client: "pg",
   connection: {
@@ -15,6 +16,31 @@ app.get("/", async (req, res) => {
   try {
     let data = await knex("users").select("*");
     res.status(200).send({ data });
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+});
+
+app.post("/add", async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    // knex.raw("insert into users (name,email) values (?,?)", [name, email]); method 1
+    await knex("users").insert({ name, email });
+    res.status(200).send({ msg: "OK" });
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+});
+
+app.patch("/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const name = req.body.name;
+    // knex.raw("UPDATE users SET name = ? WHERE id = ?", [name, id]);
+    await knex("users").update({ name }).where("id", id);
+    res.status(200).send({ msg: "OK" });
   } catch (error) {
     console.log(error.message);
     res.send(error.message);
